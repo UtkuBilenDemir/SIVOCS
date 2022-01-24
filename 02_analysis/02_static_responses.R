8b7f699d41453557e10fefb1146d51f5351651b8
Utku Demir <ubd@ubd-mbp.local>
2022-01-24 18:19:19 +0100

    Adjust Features for final tests

--- 02_analysis/02_static_responses.R
+++ 02_analysis/02_static_responses.R
@@ -16,15 +16,8 @@ colnames(data)[142:ncol(data)] <- c("persID",
 # Create a df with only the questions
 data.questions <- data[, 11:94]
 
-# Handle disciplines
-setwd("/home/ubd/Nextcloud/utku_SIVOCS")
-## disciplines <- read.csv("./DisciplinesPerFundingInstrument.csv", sep=";")
-
-
 #--- Domains
-## disc_df <- read.csv("disc_df.csv")
-## data$domain <- disc_df$domain[as.vector(sapply(data$discipline, FUN = function(x) {which(x == disc_df$discipline)}))]
-grant_df <- read.csv("./5555_preliminary_analysis/00_data/P3_GrantExport_with_abstracts.csv", sep=";")
+grant_df <- read.csv("./5555_preliminary_analysis/00_data/P3_GrantExport_with_abstracts.csv", sep = ";")
 match_grant_df <- grant_df[match(data$projID, grant_df$Project.Number), ]
 domain <- unlist(lapply(str_split(match_grant_df$Discipline.Name.Hierarchy, ";"), `[[`, 1))
 data$domain <- domain 
@@ -35,7 +28,7 @@ colnames(data)[151] <- "SSH"
 colnames(data)[152] <- "Math._Natur._and_Eng."
 
 # A df with only numerical columns
-data.num_questions <- data.questions[, as.vector(sapply(FUN=is.numeric, data.questions))]
+data.num_questions <- data.questions[, as.vector(sapply(FUN = is.numeric, data.questions))]
 
 # A df for python
 write.csv(data, "./01_data/mod_data.csv")
---
+++ 17_adjust_for_final_tests.R
@@ -0,0 +1,8 @@
+# Data Frame
+source("./02_analysis/02_static_responses.R")
+
+sapply(FUN = is.numeric, data.questions)
+
+# get the non-numeric questions and handle them
+
+
