4ca653eda84d0f75d4656ce5e1cb934e4a0043ef
Utku Demir <ubd@ubd-mbp.local>
2022-01-25 01:42:38 +0100

    Introduce fata

--- 02_analysis/02_static_responses.R
+++ 02_analysis/02_static_responses.R
@@ -2,6 +2,7 @@ library(fastDummies)
 library(stringr)
 
 data <- read.csv("./01_data/results-survey718586.csv")
+fata <- read.csv("./01_data/results-survey718586.csv", stringsAsFactors = TRUE)
 
 # name the meta variables correctly
 colnames(data)[142:ncol(data)] <- c("persID",
@@ -12,24 +13,51 @@ colnames(data)[142:ncol(data)] <- c("persID",
                                     "funding",
                                     "runtime"
                                     )
+colnames(fata)[142:ncol(data)] <- c("persID",
+                                    "gender",
+                                    "projID",
+                                    "title",
+                                    "discipline",
+                                    "funding",
+                                    "runtime"
+                                    )
+
 
 # Create a df with only the questions
 data.questions <- data[, 11:94]
+fata.questions <- fata[, 11:94]
+
 
 #--- Domains
 grant_df <- read.csv("./5555_preliminary_analysis/00_data/P3_GrantExport_with_abstracts.csv", sep = ";")
 match_grant_df <- grant_df[match(data$projID, grant_df$Project.Number), ]
 domain <- unlist(lapply(str_split(match_grant_df$Discipline.Name.Hierarchy, ";"), `[[`, 1))
 data$domain <- domain 
+fata$domain <- domain 
 
 data <- dummy_cols(data, select_columns = "domain")
+fata <- dummy_cols(fata, select_columns = "domain")
 colnames(data)[150] <- "Biology_and_Medicine"
+colnames(fata)[150] <- "Biology_and_Medicine"
 colnames(data)[151] <- "SSH"
+colnames(fata)[151] <- "SSH"
 colnames(data)[152] <- "Math._Natur._and_Eng."
+colnames(fata)[152] <- "Math._Natur._and_Eng."
+
 
 # A df with only numerical columns
 data.num_questions <- data.questions[, as.vector(sapply(FUN = is.numeric, data.questions))]
+fata.num_questions <- fata.questions[, as.vector(sapply(FUN = is.numeric, fata.questions))]
 
-# A df for python
+
+# Eliminate the open questions from fata and fata.questions
+open_questions <- c("adoptByPolicyHow.other.", "kindOfChangeOther", "impulseForNonAcad.other.")
+fata <- fata[, !c(colnames(fata) %in% open_questions)]
+fata.questions <- fata.questions[, !c(colnames(fata.questions) %in% open_questions)]
+
+
+# ------------- CSVs for python
 write.csv(data, "./01_data/mod_data.csv")
-write.csv(data.questions, "./01_data/mod_question_data.csv")
+write.csv(data, "./01_data/mod_fata.csv")
+write.csv(fata.questions, "./01_data/mod_question_data.csv")
+write.csv(fata.questions, "./01_data/mod_question_fata.csv")
--- 17_adjust_for_final_tests.R
+++ 17_adjust_for_final_tests.R
@@ -1,8 +1,55 @@
+# NOTE TO SELF: Remember, the correct and efficient way 
+# to that thing you insist to do wrongly is
+# as.numeric(levels(f))[f]
+
+
 # Data Frame
 source("./02_analysis/02_static_responses.R")
 
-sapply(FUN = is.numeric, data.questions)
+#fata.questions[fata.questions["age"] == "", "age"] <- NA
 
-# get the non-numeric questions and handle them
+# Find the factor columns
+fac_cols <- which(sapply(FUN = is.factor, fata.questions))
 
 
+# ------------ Refactor the factor columns
+# TODO: We are working with fata.questions right now, 
+# consider doing the same changes with fata
+ny <- c(0, 1)
+ony <- c(NA, 0, 1)
+nature <- c(NA, 3, 2, 0, 1)
+effects <- c(NA, 2, 1, 3, NA, 0)
+
+levels(fata.questions[, fac_cols[1]]) <- c(NA, 0, 1, 2, 3, 4)
+levels(fata.questions[, fac_cols[2]])  <- c(NA, 0, 1, 2)
+levels(fata.questions[, fac_cols[3]])  <- ny
+levels(fata.questions[, fac_cols[4]]) <- ny
+levels(fata.questions[, fac_cols[5]]) <- ny
+levels(fata.questions[, fac_cols[6]]) <- ny
+levels(fata.questions[, fac_cols[7]]) <- ny
+levels(fata.questions[, fac_cols[8]]) <- ny
+levels(fata.questions[, fac_cols[9]]) <- nature
+levels(fata.questions[, fac_cols[10]]) <- nature
+levels(fata.questions[, fac_cols[11]]) <- nature
+levels(fata.questions[, fac_cols[12]]) <- nature
+levels(fata.questions[, fac_cols[12]]) <- nature
+levels(fata.questions[, fac_cols[13]]) <- nature
+levels(fata.questions[, fac_cols[14]]) <- nature
+levels(fata.questions[, fac_cols[15]]) <- nature
+levels(fata.questions[, fac_cols[16]]) <- ony
+levels(fata.questions[, fac_cols[17]]) <- ony
+levels(fata.questions[, fac_cols[18]]) <- effects
+levels(fata.questions[, fac_cols[19]]) <- effects
+levels(fata.questions[, fac_cols[20]]) <- effects
+levels(fata.questions[, fac_cols[21]]) <- effects
+levels(fata.questions[, fac_cols[22]]) <- effects
+levels(fata.questions[, fac_cols[23]]) <- effects
+levels(fata.questions[, fac_cols[24]]) <- effects
+levels(fata.questions[, fac_cols[25]]) <- ny
+levels(fata.questions[, fac_cols[26]]) <- ny
+levels(fata.questions[, fac_cols[27]]) <- ny
+
+# Split META and FEATURE variables
+
+
+as.numeric(levels(fata.questions[, fac_cols[18]]))[fata.questions[, fac_cols[18]]]
